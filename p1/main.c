23a99248e3c555e24b6d59092c0f73d6f1e592ee
2025050214 <jeejee637@kau.kr>
2026-04-29 15:40:48 +0900

    Fix formatting issues in main.c

--- p1/main.c
+++ p1/main.c
@@ -9,6 +9,7 @@ int main (void) {
   for (int a = 1; a <= N / 900; a++){
     for (int b = 2; b <= N / 750; b+= 2){
       for (int c = 1; c <= N / 200; c++){
+        
         if (900 * a + 750 * b + 200 * c == N) {
           if (c < a || c < b) {
             printf("%d %d %d\n", a, b, c);
